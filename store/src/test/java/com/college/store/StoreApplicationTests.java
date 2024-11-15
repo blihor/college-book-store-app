51dc0794a8aa80deb26fb3ba1598f20008e203b7
Ihor <blihor@tutamail.com>
2024-11-15 13:56:06 +0200

    initial commit

---
+++ store/src/test/java/com/college/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
